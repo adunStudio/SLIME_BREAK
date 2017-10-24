580c5eee11a74458e7bb29c375037c116e65397e
adunstudio <adunstudio@daum.net>
2017-10-24 23:03:27 +0900

    kpu_credit 1280 * 720
    by 정한혁 of photoshop

--- GAME/Scene/startScene.py
+++ GAME/Scene/startScene.py
@@ -2,9 +2,8 @@ from pico2d import *
 from Framework.ADUN import Director, Scene
 from Scene.titleScene import TitleScene
 
-
 class StartScene(Scene):
-    time = 60 * 1
+    time = 60 * 2.5
 
     def enter(self):
         self.image = Director.asset['kpu_credit']
@@ -25,8 +24,8 @@ class StartScene(Scene):
         self.time -= 1
 
         if self.time <= 0:
-            self.time = 60 * 2
+            self.time = 60 * 2.5
             Director.push_scene(TitleScene())
 
     def draw(self):
-        self.image.draw(400, 300)
+        self.image.draw(Director.window_width / 2, Director.window_height / 2)
